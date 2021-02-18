d4686d0d0f9d5fe35912b82849166ff6f05ea004
Jacek <jacek.duraj44@gmail.com>
2021-02-18 17:41:22 +0100

    add statistics_printer class

---
+++ lib/statistics_printer.rb
@@ -0,0 +1,22 @@
+class StatisticsPrinter
+  attr_reader :most_viewed, :most_unique_viewed
+
+  def initialize(most_viewed:, most_unique_viewed:)
+    @most_viewed = most_viewed
+    @most_unique_viewed = most_unique_viewed
+  end
+
+  def print_most_viewed_pages
+    puts "Most Viewed Pages"
+    most_viewed.each do |key,value|
+        puts "#{key} : #{value}"
+    end
+  end
+
+  def print_most_viewed_unique_pages
+    puts "Most unique viewed pages"
+    most_unique_viewed.each do |key,value|
+        puts "#{key} : #{value}"
+    end
+  end
+end
