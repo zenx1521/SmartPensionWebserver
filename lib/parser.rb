66689fbdaee321fe092781f1e471e303ba637a00
Jacek <jacek.duraj44@gmail.com>
2021-02-18 17:36:38 +0100

    parser class implementation

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,11 +1,29 @@
 class Parser
   attr_reader :file
 
+  PATTERN = /\A(.*) (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\z/
+
   def initialize(file:)
     @file = file
   end
 
   def values
+    @values ||= parse
+  end
 
+  private
+
+  def parse
+    @values = {}
+
+    file.each do |line|
+      address, ip = line.strip.match(PATTERN).captures
+
+      @values[address] ||= {}
+      @values[address][ip] ||= 0
+      @values[address][ip] += 1
+    end
+
+    @values
   end
 end
