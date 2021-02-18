541b880879394e24c3dfd4c78124e36919e1e4af
Jacek <jacek.duraj44@gmail.com>
2021-02-18 17:40:18 +0100

    statistics_calculator class implementation

--- lib/statistics_calculator.rb
+++ lib/statistics_calculator.rb
@@ -6,8 +6,27 @@ class StatisticsCalculator
   end
 
   def most_viewed
+    calculate_results unless @most_viewed
+    @most_viewed
   end
 
   def most_unique_viewed
+    calculate_results unless @most_unique_viewed
+    @most_unique_viewed
+  end
+
+  private
+
+  def calculate_results
+    @most_viewed = {}
+    @most_unique_viewed = {}
+
+    @values.each do |key,val|
+      @most_viewed[key] = val.values.sum
+      @most_unique_viewed[key] = val.keys.count
+    end
+
+    @most_unique_viewed = @most_unique_viewed.sort_by { |key,value| [value, -key] }.reverse
+    @most_viewed = @most_viewed.sort_by { |key,value| value }.reverse
   end
 end
